8720699c3e5b126574559bec680a1c2d271443b3
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 17:40:13 -0500

    process.argv

--- flips.js
+++ flips.js
@@ -1,11 +1,12 @@
 import defaultExport from "./modules/coin.mjs";
 const process = import("process");
+let args = process.argv;
 let num;
-if (process.argv.length <= 2) {
+if (args.length <= 2) {
     num = 1;
 }
 else {
-    num = process.argv[2];
+    num = args[2];
 }
 let func = defaultExport.coinFlips;
 console.log(func(num));
