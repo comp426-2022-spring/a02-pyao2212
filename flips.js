51ce95f5fece40dc1f72bb7c0836ffd402ff7745
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 18:27:49 -0500

    again????

--- flips.js
+++ flips.js
@@ -2,10 +2,10 @@ import defaultExport from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-const num = args.number || 1
+const num = args.number || 1;
 
 let flips = defaultExport.coinFlips(num);
 console.log(flips);
 console.log(defaultExport.countFlips(flips));
-;
+
 
