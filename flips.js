23967bf9636123dc38ab68a39cb29d3e3c711536
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 18:25:21 -0500

    with minimist

--- flips.js
+++ flips.js
@@ -1,29 +1,11 @@
-/*import defaultExport from "./modules/coin.mjs";
-import process from "process";
-
-let args = process.argv;
-let num;
-if (args.length <= 2) {
-    num = 1;
-}
-else {
-    let val = args[2].slice(9);
-    num = parseInt(val);
-    if (isNaN(num)) {
-        num = 1;
-    }
-}
-let flips = defaultExport.coinFlips(num);
-console.log(flips);
-console.log(defaultExport.countFlips(flips));*/
-import { coinFlips, countFlips } from "./modules/coin.mjs";
+import defaultExport from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-const number = args.number || 1;
+const num = args.number || 1
 
-let flips = coinFlips(number)
-
+let flips = defaultExport.coinFlips(num);
 console.log(flips);
-console.log(countFlips(flips));
+console.log(defaultExport.countFlips(flips));
+;
 
