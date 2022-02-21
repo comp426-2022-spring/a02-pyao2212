f2097c2a81bcfadbd52ceef59f2402653686e1f1
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 17:57:52 -0500

    process fixed

--- flips.js
+++ flips.js
@@ -1,12 +1,18 @@
 import defaultExport from "./modules/coin.mjs";
-const process = import("process");
+import process from "process";
+
 let args = process.argv;
 let num;
 if (args.length <= 2) {
     num = 1;
 }
 else {
-    num = args[2];
+    let val = args[2].slice(9);
+    console.log(val);
+    num = parseInt(val);
+    if (isNaN(num)) {
+        num = 1;
+    }
 }
 let func = defaultExport.coinFlips;
 console.log(func(num));
