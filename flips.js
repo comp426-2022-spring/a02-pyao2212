b258fd375673e18c478899b3a5eb9ea21fddb1b4
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 21:57:45 -0500

    good?

--- flips.js
+++ flips.js
@@ -5,7 +5,16 @@ const args = minimist(process.argv.slice(2));
 const num = args.number || 1;
 
 let flips = defaultExport.coinFlips(num);
+let sum = defaultExport.countFlips(flips);
+if (flips.length == 1) {
+    if (sum.heads == 0) {
+        delete sum.heads;
+    }
+    if (sum.tails == 0) {
+        delete sum.tails;
+    }
+}
 console.log(flips);
-console.log(defaultExport.countFlips(flips));
+console.log(sum);
 
 
