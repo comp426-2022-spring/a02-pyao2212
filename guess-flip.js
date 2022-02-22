a520679f7342cf92bd7442ab48b0a56f0e64389c
Peter Yao <69826380+pyao2212@users.noreply.github.com>
2022-02-21 22:20:54 -0500

    again

--- guess-flip.js
+++ guess-flip.js
@@ -6,7 +6,7 @@ const call = args.call || "backup";
 if (call == "backup") {
     console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 } else if (call != "heads" && call != "tails") {
-    console.error("Usage: node guess-flip --call=[heads|tails]")
+    console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
     const check = defaultExport.coinFlip();
     let won = "lose";
